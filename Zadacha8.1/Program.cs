547e49740eba2d61401ec167c3d1ff637a3a4aac
Миша Мишин <spawn.rapture-0n@icloud.com>
2021-12-04 13:34:58 +0300

    Добавьте файлы проекта.

---
+++ Zadacha8.1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Zadacha8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Windows";
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("Подкаталоги:");
+                string[] directory = Directory.GetDirectories(path);
+                foreach (string s in directory)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(path);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
